cd0930117e8fd692b6999c242abf6eaa7e2f542f
Kim Holland <tripleaxis@gmail.com>
2019-09-01 08:40:24 +1000

    Added redux as per requirements
    Redux not really necessary for this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import { AppContainer } from './App.styles';
 import CustomerView from './view/CustomerView';
 
 const App = () => (
   <AppContainer>
-    <CustomerView />
+    <Provider store={store}>
+      <CustomerView />
+    </Provider>
   </AppContainer>
 );
 
--- src/view/CustomerView.tsx
+++ src/view/CustomerView.tsx
@@ -1,24 +1,25 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import CustomerSearch from './CustomerSearch';
 import CustomerList from './CustomerList';
-
-const customerData = [
-  { firstName: 'Kim', lastName: 'Bob', dob: '12/12/2001' },
-  { firstName: 'Barry', lastName: 'White', dob: '01/04/1967' },
-  { firstName: 'Augustus', lastName: 'Gloop', dob: '14/09/1974' }
-];
 
 const Title = styled.h1`
   color: #333;
 `;
 
-const CustomerView = () => (
+const CustomerView = ({ customers }) => (
   <>
-    <Title>Customer List</Title>
+    <Title>Customers</Title>
     <CustomerSearch />
-    <CustomerList customers={customerData} />
+    <CustomerList customers={customers} />
   </>
 );
 
-export default CustomerView;
+CustomerView.propTypes = {
+  customers: PropTypes.array.isRequired
+};
+
+const mapProps = ({ customers }) => ({ customers });
+export default connect(mapProps)(CustomerView);
